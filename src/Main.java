ed1c14c066d45d2a192d08b86ad48de319f723cb
Laurence <laurencearthur14@gmail.com>
2022-05-11 22:40:01 +0530

    Final Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //String name = scanner.nextLine();
+        //int year = scanner.nextInt();
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2020.");
+    }
+}
